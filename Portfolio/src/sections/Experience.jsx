02ab16d52719726c6c10f22f60a8c0dc4f137c9f
Madannadar <154582270+Madannadar@users.noreply.github.com>
2025-01-05 20:39:08 +0530

    one last step to add my avatar

--- Portfolio/src/sections/Experience.jsx
+++ Portfolio/src/sections/Experience.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { workExperiences } from '../constants'
 
 const Experience = () => {
   return (
@@ -14,7 +15,28 @@ const Experience = () => {
                 </div>
                 <div className="work-content">
                     <div className="sm:py-10 py-5 sm:px-5 px-2.5">
-                        {workexperiences.map((items))}
+                        {workExperiences.map(({id, name, pos,
+                        icon, duration, title, animation}) => (
+                            <div key={id} className='work-content_container group'>
+                                <div className="flex flex-col h-full justify-start items-center py-2">
+                                    <div className="work-content_logo">
+                                        <img src={icon} alt="logo" className='w-full h-full' />
+                                    </div>
+                                    <div className="work-content_bar" />
+                                </div>
+                                <div className="sm:p-5 px-2.5 py-5">
+                                    <p className="font-bold text-white-800">
+                                        {name}
+                                    </p>
+                                    <p className="text-sm mb-5">
+                                        {pos} --{duration}
+                                    </p>
+                                    <p className="group-hover:text-white transition ease-in-out duration-500">
+                                        {title}
+                                    </p>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
