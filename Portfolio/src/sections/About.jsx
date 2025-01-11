4e7cb4fc7a3eb091d88c1b8d4701ad5ca33edce8
Madannadar <154582270+Madannadar@users.noreply.github.com>
2025-01-11 21:35:38 +0530

    added a link to the button

--- Portfolio/src/sections/About.jsx
+++ Portfolio/src/sections/About.jsx
@@ -66,7 +66,9 @@ const About = () => {
             <div>
               <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
               <p className="grid-subtext">I&apos;m based in maharashtra, Mumbai and open to remote work worldwide.</p>
-              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+              <a href="#contact" className='w-fit'>
+                <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+              </a>
             </div>
           </div>
         </div>
