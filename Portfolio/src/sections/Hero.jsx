8fceb7a203c543defeb5bfc016d816903098c696
Madannadar <154582270+Madannadar@users.noreply.github.com>
2025-01-29 19:17:49 +0530

    changing the text in hero section and adding the dist folder

--- Portfolio/src/sections/Hero.jsx
+++ Portfolio/src/sections/Hero.jsx
@@ -65,7 +65,7 @@ const hero = () => {
     <section className='min-h-screen w-full flex flex-col relative'>
         <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
             <p className='sm:text-3xl text-xl font-medium text-white text-center font-generalsans'>Hi, I am Madan <span className='waving-hand'>👋</span></p>
-            <p className='hero_tag text-gray_gradient text-center'>hello world</p>
+            <p className='hero_tag text-gray_gradient text-center'>FullStack Developer</p>
         </div>
         <div className="w-full h-full absolute inset-0">
         {/* <Leva /> */}
